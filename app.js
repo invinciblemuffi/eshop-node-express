eca3e1ea2c56e651a00039dcb777cc6a24a0833f
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2022-01-10 21:22:13 +0530

    Add product route and body parse setup

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 // app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/product", adminRouter);
 
--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,9 @@ router.get("/add-product", (req, res) => {
   res.render("add-product", { pageTitle: "Add a Product" });
 });
 
+router.post("/addProduct", (req, res) => {
+  console.log(req.body.prodName);
+  res.redirect(302, "/");
+});
+
 module.exports = router;
